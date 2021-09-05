56a3f1ec016a7c3f6e959512e906352850440bda
Daniel Hookins <daniel@hookins.net>
2021-09-05 12:22:18 +0930

    Add player rects and adjust platforms

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+typedef struct Player {
+    Rectangle rect;
+    Color color;
+} Player;
+
 typedef struct Platform {
     Rectangle rect;
     Color color;
@@ -13,12 +18,20 @@ int main(void)
     // Init
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PLATFORM FIGHTERS!");
 
+    // Create Players
+    Player players[] = {
+        {{208, 440, 64, 128}, RED}, // Player 1
+        {{528, 440, 64, 128}, BLUE} // Player 2
+    };
+
+    int playersLength = sizeof(players) / sizeof(players[0]);
+
     // Create Platforms
     Platform platforms[] = {
         {{144, 560, 512, 160}, DARKGRAY}, // Floor Platform
-        {{208, 368, 368, 32}, DARKGRAY}, // Middle Platform
-        {{96, 208, 128, 32}, DARKGRAY}, // Top Left Platform
-        {{608, 208, 128, 32}, DARKGRAY} // Top Right Platform
+        {{208, 320, 368, 32}, DARKGRAY}, // Middle Platform
+        {{96, 144, 128, 32}, DARKGRAY}, // Top Left Platform
+        {{608, 144, 128, 32}, DARKGRAY} // Top Right Platform
     };
 
     int platformsLength = sizeof(platforms) / sizeof(platforms[0]);
@@ -36,8 +49,14 @@ int main(void)
         BeginDrawing();
             ClearBackground(SKYBLUE);
 
+            // Draw Platforms
             for (int i=0; i < platformsLength; i++) {
                 DrawRectangleRec(platforms[i].rect, platforms[i].color);
+            }
+
+            // Draw Players
+            for (int i=0; i < playersLength; i++) {
+                DrawRectangleRec(players[i].rect, players[i].color);
             }
 
         EndDrawing();
