1a832565f3f3bb43e6c7792d97f58fb2fa46b3d9
Daniel Hookins <daniel@hookins.net>
2021-09-05 12:05:36 +0930

    Add platforms

--- src/main.c
+++ src/main.c
@@ -3,11 +3,27 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+typedef struct Platform {
+    Rectangle rect;
+    Color color;
+} Platform;
+
 int main(void)
 {
     // Init
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PLATFORM FIGHTERS!");
 
+    // Create Platforms
+    Platform platforms[] = {
+        {{144, 560, 512, 160}, DARKGRAY}, // Floor Platform
+        {{208, 368, 368, 32}, DARKGRAY}, // Middle Platform
+        {{96, 208, 128, 32}, DARKGRAY}, // Top Left Platform
+        {{608, 208, 128, 32}, DARKGRAY} // Top Right Platform
+    };
+
+    int platformsLength = sizeof(platforms) / sizeof(platforms[0]);
+
+    // Set Target FPS
     SetTargetFPS(60);
 
     // Main Game Loop
@@ -18,8 +34,12 @@ int main(void)
 
         // Draw
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawText("Hello platform fighters!", 100, 100, 28, WHITE);
+            ClearBackground(SKYBLUE);
+
+            for (int i=0; i < platformsLength; i++) {
+                DrawRectangleRec(platforms[i].rect, platforms[i].color);
+            }
+
         EndDrawing();
     }
 
